dfd9459e78165ce22121c17d283db686d6082ed1
Brian Grant and Terri Bradley <brian+terri@>
2016-08-16 13:20:01 -0700

    add ability to delete a story from dataase

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,10 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+     return this.store.findAll('newsitem');
+  },
   actions: {
     save3(params) {
       var newNewsItem = this.store.createRecord('newsitem', params);
       newNewsItem.save();
+      this.transitionTo('admin');
+    },
+
+    destroyNewsItem(newsitem) {
+      newsitem.destroyRecord();
       this.transitionTo('admin');
     }
   }
